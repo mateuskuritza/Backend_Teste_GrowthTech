d38b2356e5f8e9c2b961a1122d36172367377b36
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-07-08 23:54:51 -0300

    route get posts responding with username and company

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -4,8 +4,22 @@ import fetch from "node-fetch";
 const postController = {
 	async getAllPosts(req: Request, res: Response) {
 		try {
-			const response = (await fetch("http://jsonplaceholder.typicode.com/posts")).json();
-			res.send(await response);
+			const posts = await (await fetch("http://jsonplaceholder.typicode.com/posts")).json();
+			const users = await (await fetch("http://jsonplaceholder.typicode.com/users")).json();
+			const response = posts.map((p) => {
+				const user = users.find((u) => u.id === p["userId"]);
+				const obj = {
+					userId: user.id,
+					postId: p.id,
+					title: p.title,
+					body: p.body,
+					name: user.name,
+					company: user.company.name,
+				};
+				return obj;
+			});
+
+			res.send(response);
 		} catch {
 			res.sendStatus(500);
 		}
